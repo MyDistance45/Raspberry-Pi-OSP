93d0d983130d93d3aabc0361c03ef0b73e6347ff
MyDistance45 <168000087+MyDistance45@users.noreply.github.com>
2025-06-03 19:02:12 +0800

    Update submit_order.php

--- submit_order.php
+++ submit_order.php
@@ -2,11 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// Database config
+
 $host = 'localhost';
 $db = 'order_track';
 $user = 'root';
-$pass = ''; // or your MariaDB root password
+$pass = 'root';
 
 // Connect to database
 $conn = new mysqli("localhost", "root", "root", "order_track_db");
